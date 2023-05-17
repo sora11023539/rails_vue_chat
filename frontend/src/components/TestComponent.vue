6c53c3804afdd44f83ecf089497e5799b2a4f8d7
sora11023539 <sora11023539@gmail.com>
2023-05-18 00:41:00 +0900

    add: Create components

---
+++ frontend/src/components/TestComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    This is appeared from component
+    <p>{{ message }}</p>
+    <button @click="onClick">Execute parent method button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'message',
+    'isEnabled'
+  ],
+
+  methods: {
+    onClick() {
+      this.$emit('toggle')
+    }
+  }
+}
+</script>
