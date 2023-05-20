f16a3e5f36428131e062f756c2d003135119624f
sora11023539 <sora11023539@gmail.com>
2023-05-21 01:22:49 +0900

    add: implement login

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,21 +1,46 @@
 <template>
   <div>
     <h2>Login</h2>
-    <form>
+    <form @submit.prevent="login">
       <input type="email" required placeholder="Mail address" v-model="email" />
       <input type="password" required placeholder="Password" v-model="password" />
+      <div class="error">{{ error }}</div>
       <button>Login</button>
     </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
       email: '',
       password: '',
+      error: null,
     };
+  },
+
+  methods: {
+    async login() {
+      try {
+        this.error = null;
+
+        const res = await axios.post('http://localhost:8000/auth/sign_in', {
+          email: this.email,
+          password: this.password,
+        });
+        if (!res) {
+          throw new Error('Incorrect email or password');
+        }
+        console.log({ res });
+        return res;
+      } catch (error) {
+        console.log({ error });
+        this.error = 'Incorrect email or password';
+      }
+    },
   },
 };
 </script>
