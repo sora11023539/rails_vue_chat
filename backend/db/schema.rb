77f18beb788057bb18ddd123568b88fc251c7b89
sora11023539 <sora11023539@gmail.com>
2023-05-20 17:30:16 +0900

    add: create message model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_20_003639) do
+ActiveRecord::Schema.define(version: 2023_05_20_082611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
