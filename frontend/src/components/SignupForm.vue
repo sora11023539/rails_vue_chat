573776893d038216843ee4146e7caab269384d07
sora11023539 <sora11023539@gmail.com>
2023-05-20 23:01:18 +0900

    add: axios

--- frontend/src/components/SignupForm.vue
+++ frontend/src/components/SignupForm.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <h2>Sign up</h2>
-    <form>
+    <form @submit.prevent="signUp">
       <input type="text" required placeholder="Name" v-model="name" />
       <input type="email" required placeholder="Email" v-model="email" />
       <input type="password" required placeholder="Password" v-model="password" />
       <input type="password" required placeholder="Password confirmation" v-model="passwordConfirmation" />
+      <div class="error">{{ error }}</div>
       <button>Sign up</button>
     </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
@@ -19,7 +22,29 @@ export default {
       email: '',
       password: '',
       passwordConfirmation: '',
+      error: null,
     };
+  },
+
+  methods: {
+    async signUp() {
+      this.error = null;
+      try {
+        const res = await axios.post('http://localhost:8000/auth', {
+          name: this.name,
+          email: this.email,
+          password: this.password,
+          password_confirmation: this.password_confirmation,
+        });
+        if (!res) {
+          throw new Error('Could not signup')
+        }
+        console.log({ res });
+        return res;
+      } catch (error) {
+        this.error = 'Could not signup'
+      }
+    },
   },
 };
 </script>
