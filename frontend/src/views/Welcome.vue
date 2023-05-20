5f7fc178995e4e60ef80690f35baa941df9e8b22
sora11023539 <sora11023539@gmail.com>
2023-05-20 22:06:24 +0900

    add: styling welcome page

--- frontend/src/views/Welcome.vue
+++ frontend/src/views/Welcome.vue
@@ -1,48 +1,14 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <TestComponent @toggle="toggle" message="Give data for component" />
-    <p v-if="isEnabled">hello</p>
+  <div class="container welcome">
+    <p>Welcome!</p>
   </div>
 </template>
 
-<script>
-import TestComponent from "@/components/TestComponent.vue";
-
-export default {
-  components: {
-    TestComponent
-  },
-
-  data() {
-    return {
-      title: 'New vue app',
-      subtitle: 'ようこそ',
-      isEnabled: true,
-    };
-  },
-
-  computed: {
-    text() {
-      if (this.isEnabled) {
-        return 'hello'
-      } else {
-        return 'bye'
-      }
-    }
-  },
-
-  methods: {
-    toggle() {
-      this.isEnabled = !this.isEnabled
-    }
-  }
-};
-</script>
+<script></script>
 
 <style scoped>
-  p {
-    font-size: 20px;
-    color: blue;
-  }
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
 </style>
