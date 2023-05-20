fb15018a9d73c37dd780ff7d2770098237993368
sora11023539 <sora11023539@gmail.com>
2023-05-20 22:28:07 +0900

    add: login form

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form>
+      <input type="email" required placeholder="Mail address" v-model="email" />
+      <input type="password" required placeholder="Password" v-model="password" />
+      <button>Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+};
+</script>
